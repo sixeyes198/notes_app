37b2780527667310ae559e7e50448cf7b9701aa9
sixeyes198 <yossiguadie@icloud.com>
2024-11-21 15:02:41 +0200

    working on the login functionalty

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
+import PasswordInput from "../../components/Input/PasswordInput";
 
 const Login = () => {
+  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
   return (
     <>
       <Navbar />
@@ -11,10 +17,15 @@ const Login = () => {
         <div className="w-96 border rounded bg-white px-7 py-10">
           <form onSubmit={() => {}}>
             <h4 className="text-2xl mb-7">Login</h4>
+
             <input type="text" placeholder="Email" className="input-box" />
+
+            <PasswordInput />
+
             <button type="submit" className="btn-primary">
               Login
             </button>
+
             <p className="text-sm text-center mt-4">
               Not registered?{" "}
               <Link to="/signup" className="font-medium text-primary underline">
