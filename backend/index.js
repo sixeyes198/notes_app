4b44129936d60d2e269623666aca8bc34d0525e9
sixeyes198 <yossiguadie@icloud.com>
2024-12-10 16:59:01 +0200

    update ,continue after the DB connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
-import express from "express";
-import cors from "cors";
+
+require("dotenv").config();
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+// mongoose.connect(config.connectionString);
+mongoose.connect(config.connectionString).then(()=>{
+  console.log("Connected to MongoDB");
+}).catch((err)=>{
+  console.log("Error connecting to MongoDB")
+})
+
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({ 
+  origin: "*"
+ }));
 
 app.get("/", (req, res) => {
   res.json({ data: "Hey" });
--- backend/utilities.js
+++ backend/utilities.js
@@ -1,13 +1,15 @@
-import jwt from "jsonwebtoken"
+const jwt = require("jsonwebtoken");
 
-export function authenticateToken(req,res,next){
-    const authHeader = req.headers["authorization"]
-    const token = authHeader && authHeader.split(" ")[1]
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    if (!token) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err, user)=>{
-        if(err) return res.sendStatus(403)
-            req.user = user;
-        next();
-    });
+  if (!token) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 }
+
+module.exports = authenticateToken;
