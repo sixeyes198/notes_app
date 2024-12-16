3d0facf7b3d1008ba4462e3db27ec098d4e6bc68
sixeyes198 <yossiguadie@icloud.com>
2024-12-16 17:02:38 +0200

    update

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,24 +1,36 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 
-const routes = (
-  <Router>
-    <Routes>
-      <Route path="/dashboard" exact element={<Home />} />
-      <Route path="/login" exact element={<Login />} />
-      <Route path="/signup" exact element={<SignUp />} />
-    </Routes>
-  </Router>
-);
+// const routes = (
+//   <Router>
+//     <Routes>
+//     <Route path="*" element={<Navigate to="/signup" />} />
+//       <Route path="/dashboard" exact element={<Home />} />
+//       <Route path="/login" exact element={<Login />} />
+//       <Route path="/signup" exact element={<SignUp />} />
+//     </Routes>
+//   </Router>
+// );
 
 const App = () => {
   return (
-  <>
-  {routes}
-  </>
+    <Router>
+      <Routes>
+        <Route path="*" element={<Navigate to="/signup" />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 };
+
 export default App;
